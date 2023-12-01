22b3802d3f4817ca6b4833fec95f99ea36bd373e
Arjun <arjun346002@gmail.com>
2023-12-01 16:02:08 +0530

    sec

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,70 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+
 const Body = () => {
-  const [list , setList] = useState(resList);
-  return (
+  const [list, setList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.763089020178832&lng=77.26507069360963&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setList(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilterList(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return list.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
-         const filteredList = list.filter((res) => res.info.avgRating > 4);
-          // console.log(filteredList);
-          setList(filteredList);
-        }}>
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = list.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilterList(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = list.filter((res) => res.info.avgRating > 4);
+            setFilterList(filteredList);
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
       <hr />
       <div className="res-container">
-        {list.map((restaurant) => (
+        {filterList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
-    </div>
+    </div>  
   );
 };
 
